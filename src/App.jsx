e95b0539e3036e4f5d58c3dd6a143a93a9ba8872
Javier Veron <javier.veron@gmail.com>
2024-05-09 19:48:09 -0300

    Clase 10

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Abstraccion1 from './Clase10/Abstraccion1'
+import Abstraccion2 from './Clase10/Abstraccion2'
+/* import Intercambiabilidad from './Clase10/Intercambiabilidad' */
+
+function App() {
+  return (
+    <>
+      <Abstraccion2 />
+    </>
+  )
+}
+
+export default App
